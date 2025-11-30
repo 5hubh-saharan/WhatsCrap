f699864bdaa0b755413bf97ba41338cace5fb990
5hubh-saharan <ssaharan2@myseneca.ca>
2025-11-30 18:32:05 -0500

    fix models

--- app/models/message.py
+++ app/models/message.py
@@ -1,17 +1,19 @@
-from sqlalchemy import Column, TIMESTAMP, TEXT, ForeignKey, func
-from sqlalchemy.orm import relationship
+from sqlalchemy import String, func, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID
 from app.database.base import Base
+from datetime import datetime
+import uuid
 
 
 class Message(Base):
     __tablename__ = 'messages'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
-    content = Column(TEXT)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
-    room_id = Column(UUID(as_uuid=True), ForeignKey('chatrooms.id'))
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    content: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey('users.id'))
+    room_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey('chatrooms.id'))
 
-    user = relationship("User", back_populates="messages")
-    room = relationship("ChatRoom", back_populates="messages")
+    user: Mapped["User"] = relationship(back_populates="messages")
+    room: Mapped["ChatRoom"] = relationship(back_populates="messages")
