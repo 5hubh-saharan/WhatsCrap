9a0d689304b61390cd40b977a0f2d491c09e4afb
5hubh-saharan <ssaharan2@myseneca.ca>
2025-12-02 21:36:22 -0500

    modify config to make render happy

--- app/config.py
+++ app/config.py
@@ -11,3 +11,13 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+# Convert Render’s sync DB URL to async one
+if settings.DATABASE_URL.startswith("postgres://"):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace(
+        "postgres://", "postgresql+asyncpg://", 1
+    )
+elif settings.DATABASE_URL.startswith("postgresql://"):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace(
+        "postgresql://", "postgresql+asyncpg://", 1
+    )
